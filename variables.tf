7b33e71ec6e109c0c5dba41c03cb1ea9053fc952
Ashish20004 <yandraashish996@gmail.com>
2026-08-25 10:08:34 +0530

    creating private&database subnets

--- variables.tf
+++ variables.tf
@@ -37,3 +37,25 @@ variable "public_subnet_tags"{
     type = map
     default = {}
 }
+
+variable "private_subnets_cidr" {
+    type = list
+    default = ["10.0.11.0/24","10.0.12.0/24"]
+}
+
+variable "private_subnet_tags"{
+    type = map
+    default = {}
+}
+
+
+variable "database_subnets_cidr"{
+    type = list
+    default = ["10.0.21.0/24", "10.0.22.0./24"]
+}
+
+variable "database_subnet_tags"{
+    type = map
+    default = {}
+}
+
