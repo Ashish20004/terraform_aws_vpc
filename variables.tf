e5b3e70dc0cbbe2a6d6a8a255d0dc6c8d6ede1b3
Ashish20004 <yandraashish996@gmail.com>
2026-08-25 07:40:11 +0530

    aws_vpc_creation

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr" {
+    type  = string
+    default = "10.0.0.0/16"
+}
+
+variable "environment" {
+    type  = string
+
+    validation {
+        condition    = contains(["dev","prod","uat","qa"], var.environment)
+        error_message = "Environments should be one of dev, qa, uat or prod"
+    }
+}
+
+variable "project" {
+    type  = string
+}
+
+variable "vpc_tags" {
+    type = map
+    default = {}
+}
+
